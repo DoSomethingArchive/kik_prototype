3be09a7b727bac788f75ebed657d6337f9fd2aa4
Jon Uy <jonxuy@gmail.com>
2013-12-06 16:29:53 -0500

    Server retrieves listen port from environment var fi available

--- server.js
+++ server.js
@@ -223,7 +223,7 @@ app.configure(function() {
 });
 
 // Start server
-var port = 4711;
+var port = process.env.PORT || 4711;
 app.listen(port, function() {
   console.log('Express server listening on port %d in %s mode...\n\n', port, app.settings.env);
 });
